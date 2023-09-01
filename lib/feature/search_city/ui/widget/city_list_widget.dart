65b9b79ad3c6b3f1ea928502d3f2297d9a9ae862
Gen-LJ <flashseeker1998@gmail.com>
2023-09-01 19:30:33 +0630

    flag added in city search ui

--- lib/feature/search_city/ui/widget/city_list_widget.dart
+++ lib/feature/search_city/ui/widget/city_list_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:riverpod_weather_app/feature/search_city/data/model/city_search_result.dart';
 
@@ -15,23 +16,36 @@ class CityList extends StatelessWidget {
           Results? results = cities?[index];
           return Card(
             child: Row(
-              children: [Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(results?.name ?? ''),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(results?.country ?? ''),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(results?.admin1 ?? ''),
-                  )
-                ],
-              )],
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(results?.name ?? ''),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(results?.country ?? ''),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(results?.admin1 ?? ''),
+                    )
+                  ],
+                ),
+                CachedNetworkImage(
+                    imageUrl:
+                    'https://flagsapi.com/${results?.countryCode}/flat/64.png',
+                  width: 64,
+                  height: 64,
+                  progressIndicatorBuilder: (_,__,___) => const Center(child: CircularProgressIndicator(), ),
+                    errorWidget: (_,__,___) => const Center(child: Icon(Icons.error,color: Colors.red,),)
+
+                )
+
+          ],
             ),
           );
         });
